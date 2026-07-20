79b73123df2dbbdcc7e1ece4bf88cd2381d93a2b
Milan Jindal <89682834+Milan4336@users.noreply.github.com>
2026-07-20 11:40:06 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (69.33%) - LeetSync

---
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.java
@@ -0,0 +1,36 @@
+
+class MyQueue {
+    Stack<Integer> s1;
+    Stack<Integer> s2;
+
+    public MyQueue() {
+        s1 = new Stack<>();
+        s2 = new Stack<>();
+    }
+
+    public void push(int x) {
+        s1.push(x);
+    }
+
+    private void move() {
+        if (s2.isEmpty()) {
+            while (!s1.isEmpty()) {
+                s2.push(s1.pop());
+            }
+        }
+    }
+
+    public int pop() {
+        move();
+        return s2.pop();
+    }
+
+    public int peek() {
+        move();
+        return s2.peek();
+    }
+
+    public boolean empty() {
+        return s1.isEmpty() && s2.isEmpty();
+    }
+}
